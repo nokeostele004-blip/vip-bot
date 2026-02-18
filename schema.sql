bcefc82e82b8ad9c85f4c2667db20c3cf38bfb1b
nokeostele004-blip <nokeostele004@gmail.com>
2026-02-18 14:32:44 +0700

    Add tables for groups, subscriptions, and transactions

--- schema.sql
+++ schema.sql
@@ -1,1 +1,24 @@
 
+CREATE TABLE groups (
+  telegram_group_id TEXT PRIMARY KEY,
+  name TEXT,
+  price_1d INTEGER,
+  price_7d INTEGER,
+  price_30d INTEGER
+);
+
+CREATE TABLE subscriptions (
+  telegram_user_id TEXT,
+  telegram_group_id TEXT,
+  expire_at INTEGER,
+  PRIMARY KEY (telegram_user_id, telegram_group_id)
+);
+
+CREATE TABLE transactions (
+  order_id TEXT PRIMARY KEY,
+  telegram_user_id TEXT,
+  telegram_group_id TEXT,
+  duration TEXT,
+  amount INTEGER,
+  status TEXT
+);
